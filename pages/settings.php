4ea87db806e4731d5c8d83d134a45678a779da8e
Jan Kristinus <jan.kristinus@yakamara.de>
2025-10-23 19:04:51 +0200

    binary pfad check eingebaut

--- pages/settings.php
+++ pages/settings.php
@@ -2,6 +2,8 @@
 
 /** @var rex_addon $this */
 
+use Symfony\Component\Process\Process;
+
 $func = rex_request('func', 'string');
 
 if ('update' === $func) {
@@ -12,11 +14,43 @@ if ('update' === $func) {
                 [
                     'binaryPath', 'string',
                 ],
-            ]
-        )
+            ],
+        ),
     );
 
     echo rex_view::success($this->i18n('settings_saved'));
+}
+
+// führe einen konsolenaufruf über symfony aus, um zu prüfen, ob der aspell-pfad korrekt ist. bitte nur wirklich vorhandene klassen verwenden
+$binaryPath = $this->getConfig('binaryPath');
+
+$aspellCheck = false;
+if ($binaryPath) {
+    $process = new Process([$binaryPath, '--version']);
+    try {
+        $process->run();
+
+        if ($process->isSuccessful()) {
+            $aspellCheck = true;
+        }
+    } catch (Exception $e) {
+        $aspellCheckMessage = rex_view::error('Fehler beim Ausführen von Aspell: ' . rex_escape($e->getMessage()));
+    }
+}
+
+if ($aspellCheck) {
+    echo rex_view::info('Aspell-Pfad ist gültig: ' . rex_escape($binaryPath));
+} else {
+    echo rex_view::error('Aktueller Aspell-Pfad ist nicht gültig: ' . rex_escape($binaryPath));
+
+    exec('which aspell', $output, $returnCode);
+
+    if (0 === $returnCode && !empty($output)) {
+        $aspellPath = $output[0];
+        echo rex_view::info('Folgender Aspell-Pfad wurde automatisch gefunden: `' . rex_escape($aspellPath) . '`. Bitte diesen in den Einstellungen übernehmen.');
+    } else {
+        echo rex_view::error('Es wurde kein Aspell-Pfad automatisch gefunden');
+    }
 }
 
 $content = '';
@@ -24,28 +58,28 @@ $content = '';
 $formElements = [];
 
 $n = [];
-$n['label'] = '<label for="rex-id-lang">' . rex_i18n::msg('spellchecker_bin_path') . '</label>';
+$n['label'] = '<label for="spellchecker-bin">' . rex_i18n::msg('spellchecker_bin_path') . '</label>';
 $n['field'] = '<input class="form-control" id="spellchecker-bin" type="text" name="spellchecker[binaryPath]" value="' . rex_escape($this->getConfig('binaryPath')) . '" />';
-$n['note'] = '/usr/bin/aspell';
+$n['note'] = 'Default Aspell Pfad: `/usr/bin/aspell`';
 $formElements[] = $n;
 
-$fragment = new \rex_fragment();
+$fragment = new rex_fragment();
 $fragment->setVar('elements', $formElements, false);
 $content .= $fragment->parse('core/form/form.php');
 
 $formElements = [];
 $n = [];
-$n['field'] = '<button class="btn btn-apply rex-form-aligned" type="submit" name="send" value="1"'.\rex::getAccesskey(
-    \rex_i18n::msg('update'),
-    'apply'
-).'>'.\rex_i18n::msg('update').'</button>';
+$n['field'] = '<button class="btn btn-apply rex-form-aligned" type="submit" name="send" value="1"' . rex::getAccesskey(
+        rex_i18n::msg('update'),
+        'apply',
+    ) . '>' . rex_i18n::msg('update') . '</button>';
 $formElements[] = $n;
 
-$fragment = new \rex_fragment();
+$fragment = new rex_fragment();
 $fragment->setVar('elements', $formElements, false);
 $buttons = $fragment->parse('core/form/submit.php');
 
-$fragment = new \rex_fragment();
+$fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
 $fragment->setVar('title', $this->i18n('settings'), false);
 $fragment->setVar('body', $content, false);
@@ -53,8 +87,8 @@ $fragment->setVar('buttons', $buttons, false);
 $section = $fragment->parse('core/page/section.php');
 
 echo '
-    <form action="'.\rex_url::currentBackendPage().'" method="post">
+    <form action="' . rex_url::currentBackendPage() . '" method="post">
         <input type="hidden" name="func" value="update" />
-        '.$section.'
+        ' . $section . '
     </form>
 ';
